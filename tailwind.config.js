76718b450b917d27976f87c95170a1e0ea0deec7
GCgang <powm97@gmail.com>
2024-08-14 14:38:39 +0900

    Feat: brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#F96162',
+      },
+    },
   },
   plugins: [],
 };

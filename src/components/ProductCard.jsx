f111c97484fac98aaea39b511a3a374e44c423b0
GCgang <powm97@gmail.com>
2024-08-21 18:37:21 +0900

    Feat: Product Card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,14 @@
+export default function ProductCard({
+  product: { id, image, title, category, price },
+}) {
+  return (
+    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+      <img className='w-full h-4/5' src={image} alt={title} />
+      <div className='mt-2 px-2 text-lg flex justify-between items-center'>
+        <h3 className='truncate'>{title}</h3>
+        <p>{`₩${price}`}</p>
+      </div>
+      <p className='mb-2 px-2 text-gray-600'>{category}</p>
+    </li>
+  );
+}

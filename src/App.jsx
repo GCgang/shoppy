7a497b92861bcb987937405d1177ed58f93db3e0
GCgang <powm97@gmail.com>
2024-08-21 18:37:05 +0900

    Feat: query client provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NavBar from './components/Navbar';
 import { AuthContextProvider } from './components/context/AuthContext';
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <AuthContextProvider>
-      <NavBar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <NavBar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 

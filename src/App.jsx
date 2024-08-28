8c6a4536c99c102b0af58e59ca19fa73238e60bd
GCgang <powm97@gmail.com>
2024-08-28 18:40:21 +0900

    Rename:  설정 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NavBar from './components/Navbar';
-import { AuthContextProvider } from './context/AuthContext';
 
-const queryClient = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <NavBar />
-        <Outlet />
-      </AuthContextProvider>
-    </QueryClientProvider>
+    <>
+      <NavBar />
+      <Outlet />
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );

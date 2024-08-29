7436daf1193c5353cec10136fd3648364fb6b4a1
GCgang <powm97@gmail.com>
2024-08-29 20:10:32 +0900

    Refactor: Button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,6 +1,16 @@
-export default function Button({ text, onClick }) {
+export default function Button({
+  text,
+  onClick,
+  icon: Icon,
+  bgColor = 'bg-brand',
+  textColor = 'text-white',
+}) {
   return (
-    <button className='bg-brand text-white p-1 rounded-sm' onClick={onClick}>
+    <button
+      className={`${bgColor} ${textColor} flex items-center justify-center gap-2 p-2 w-full`}
+      onClick={onClick}
+    >
+      {Icon && <Icon />}
       {text}
     </button>
   );

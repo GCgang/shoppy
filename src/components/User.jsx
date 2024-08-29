fedb42829fce6ba5bbddbb3ae71cc5f7057a19aa
GCgang <powm97@gmail.com>
2024-08-29 20:10:46 +0900

    Feat: user 기본 이미지

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,17 @@
 export default function User({ user: { photoURL, displayName } }) {
+  const defaultImage =
+    'https://via.placeholder.com/150/000000/FFFFFF/?text=User';
   return (
     <div className='flex items-center'>
       <img
         className='w-10 h-10 rounded-full mr-2'
-        src={photoURL}
+        src={photoURL || defaultImage}
         alt={displayName}
         referrerPolicy='no-referrer'
       />
-      <span className='hidden md:block'>{displayName}</span>
+      <span className='hidden md:block  whitespace-nowrap  text-ellipsis max-w-xs'>
+        {displayName}
+      </span>
     </div>
   );
 }

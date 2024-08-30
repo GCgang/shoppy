85ff9f070095a1af32ff09b050314b16d934043a
GCgang <powm97@gmail.com>
2024-08-30 10:07:23 +0900

    Feat: profile 정보 있는 경우만 보여주기

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,17 +1,19 @@
 export default function User({ user: { photoURL, displayName } }) {
-  const defaultImage =
-    'https://via.placeholder.com/150/000000/FFFFFF/?text=User';
   return (
     <div className='flex items-center'>
-      <img
-        className='w-10 h-10 rounded-full mr-2'
-        src={photoURL || defaultImage}
-        alt={displayName}
-        referrerPolicy='no-referrer'
-      />
-      <span className='hidden md:block  whitespace-nowrap  text-ellipsis max-w-xs'>
-        {displayName}
-      </span>
+      {photoURL && (
+        <img
+          className='w-10 h-10 rounded-full mr-2'
+          src={photoURL}
+          alt={displayName}
+          referrerPolicy='no-referrer'
+        />
+      )}
+      {displayName && (
+        <span className='hidden md:block  whitespace-nowrap  text-ellipsis max-w-xs'>
+          {displayName}
+        </span>
+      )}
     </div>
   );
 }

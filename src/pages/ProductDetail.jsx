a7f88a403469ced89ba22502a473692584f04f8c
GCgang <powm97@gmail.com>
2024-08-30 11:24:08 +0900

    Feat: 로그인 후 장바구니 추가

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '../components/ui/Button';
 import useCart from '../hooks/useCart';
+import { useAuthContext } from '../context/AuthContext';
 
 export default function ProductDetail() {
   const { addOrUpdateItem } = useCart();
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
   const {
     state: {
       product: { id, image, title, description, category, price, options },
@@ -29,13 +32,13 @@ export default function ProductDetail() {
         <div className='w-full px-4 basis-7/12'>
           <img src={image} alt={title} />
         </div>
-        <div className='w-full basis-5/12 flex flex-col p-4'>
+        <div className='w-full basis-5/12 flex flex-col p-4 gap-2'>
           <h2 className='text-3xl font-bold py-2'>{title}</h2>
           <p className='text-2xl font-bold py-2  border-b border-gray-400'>
-            ₩{price}
+            {price.toLocaleString()}원
           </p>
           <p className='py-4 text-lg'>{description}</p>
-          <div className='flex items-center'>
+          <div className='flex items-center mb-4'>
             <label className='text-brand font-bold' htmlFor='select'>
               옵션:
             </label>
@@ -51,8 +54,26 @@ export default function ProductDetail() {
                 ))}
             </select>
           </div>
-          {success && <p className='my-2'>✅ {success}</p>}
-          <Button text='장바구니에 추가' onClick={handleClick} />
+          <div className='flex flex-col gap-2'>
+            {success && <p className='my-2'>✅ {success}</p>}
+            <Button
+              text='BUY NOW'
+              bgColor='bg-black'
+              onClick={() => {
+                alert('준비중 입니다.');
+              }}
+            ></Button>
+            {user ? (
+              <Button text='장바구니에 추가' onClick={handleClick} />
+            ) : (
+              <Button text='Log in' onClick={() => navigate('/login')} />
+            )}
+            {!user && (
+              <p className='py-4 text-lg'>
+                로그인 후 장바구니에 추가할 수 있습니다.
+              </p>
+            )}
+          </div>
         </div>
       </section>
     </>
